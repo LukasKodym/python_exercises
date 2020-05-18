49eee771a595ab62b0254335705aca4f288b0af6
Lukas <lukasz.kodym@gmail.com>
2020-05-18 21:21:30 +0200

    warm-up done

---
+++ part_1/print_function.py
@@ -0,0 +1,54 @@
+# %%
+##
+print('Ucz się Pythona!')
+
+# %%
+##
+age = 20
+print(f'Mam {age} lat.')
+
+# %%
+##
+str_1 = 'Python'
+str_2 = '3.8'
+print(f'Uczę się języka {str_1} w wersji {str_2}')
+
+# %%
+##
+price = 199.99
+print('Towar kosztuje', price)
+
+# %%
+##
+price = 69.99
+print(f'Towar kosztuje {price}.')
+
+# %%
+##
+price = 34.99
+weight = 10
+print('Cena: {} PLN. Waga: {} kg'.format(price, weight))
+print(f'Cena: {price} PLN. Waga: {weight} kg')
+
+# %%
+##
+pi = 3.1415926535
+print('Przybliżenie liczby pi do części dziesiętnych: {}'.format(round(pi, 2)))
+print(f'Przybliżenie liczby pi do części dziesiętnych: {pi:.2f}')
+
+# %%
+##
+print('-' * 40)
+print('WERSJA: 1.0.1')
+print('-' * 40)
+
+# %%
+##
+print('=' * 40)
+print('autor: jannowak@poczta.com')
+print('data: 01-01-2021')
+print('=' * 40)
+
+# %%
+##
+print('summer', 'time', 'holiday', sep='#')
