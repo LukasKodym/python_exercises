8bbb4e43bbb8986889ada885fa41fc639cd21d20
Lukas <lukasz.kodym@gmail.com>
2020-05-20 19:50:07 +0200

    slices and types done

---
+++ part_3/py_slices.py
@@ -0,0 +1,20 @@
+# %%
+##
+filename = 'view.jpg'
+print(filename[5:])
+
+# %%
+##
+string = 'PKV-89415-PLN'
+print(string[:3] + string[10:])
+
+# %%
+##
+string = '1 0 0 1 0 1'
+val = string[::2]
+print(f'Znaleziona liczba: {int(val, 2)}')
+
+# %%
+##
+text = 'Kurs Python'
+print(text[::-1])
