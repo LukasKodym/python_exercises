c86288fe2fc3f82e26d296016b22c231d1e476f0
Lukas <lukasz.kodym@gmail.com>
2020-05-21 09:51:54 +0200

    string methods in progress

---
+++ part_5/methods.py
@@ -0,0 +1,16 @@
+# %%
+##
+text = 'python jest popularnym językiem programowania.'
+print(text.capitalize())
+
+# %%
+##
+text = 'python jest popularnym językiem programowania.'
+# p = text.count('p')
+print(f'Liczba wystąpień: {text.count("p")}')
+
+# %%
+##
+code1 = 'FVNISJND-XX-2020'
+code2 = 'FVNISJND-XY-2019'
+print(f'code1: {code1.endswith("2020")}\ncode2: {code2.endswith("2020")}')
