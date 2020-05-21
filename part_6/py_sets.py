60194649c00484696096bc6ed17841f899fd715e
Lukas <lukasz.kodym@gmail.com>
2020-05-21 22:40:54 +0200

    sets in progress

--- part_6/py_sets.py
+++ part_6/py_sets.py
@@ -8,3 +8,19 @@ print(przedmioty)
 ##
 text = 'Programming in python.'
 vowels = {'a', 'e', 'i', 'o', 'u'}
+val = set(text.lower())
+print(val)
+val.remove(' ')
+print(val)
+y = val.remove('.')
+print(val)
+x = val.intersection(vowels)
+print(x)
+val.remove(x)
+print(val)
+
+# %%
+##
+A = {1, 2, 3, 4, 5, 6, 7}
+B = {5, 6, 7, 8, 9}
+C = {5, 6}
