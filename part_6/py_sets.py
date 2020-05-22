d7b7f7c0d128c5610926d8813d2e405ab272a72a
Lukas <lukasz.kodym@gmail.com>
2020-05-22 08:45:10 +0200

    sets done

--- part_6/py_sets.py
+++ part_6/py_sets.py
@@ -9,18 +9,37 @@ print(przedmioty)
 text = 'Programming in python.'
 vowels = {'a', 'e', 'i', 'o', 'u'}
 val = set(text.lower())
-print(val)
 val.remove(' ')
-print(val)
-y = val.remove('.')
-print(val)
-x = val.intersection(vowels)
-print(x)
-val.remove(x)
-print(val)
+val.remove('.')
+val.difference_update(vowels)
+print(f'Liczba elementów: {len(val)}')
+
+# or
+# %%
+##
+text = 'Programming in python.'
+vowels = {'a', 'e', 'i', 'o', 'u'}
+letters = set(text.lower().replace(' ', '').replace('.', ''))
+letters.difference_update(vowels)
+print(f'Liczba elementów: {len(letters)}')
 
 # %%
 ##
-A = {1, 2, 3, 4, 5, 6, 7}
-B = {5, 6, 7, 8, 9}
-C = {5, 6}
+A = {2, 4, 6, 8}
+B = {4, 10}
+C = A.symmetric_difference(B)
+print(f'Róznica symetryczna: {C}')
+
+# %%
+##
+ad1_id = {'001', '002', '003'}
+ad2_id = {'002', '003', '007'}
+add = ad1_id.symmetric_difference(ad2_id)
+print(f'Wybrane ID: {add}')
+
+# %%
+##
+is_clicked = {'9001', '9002', '9005'}
+is_bought = {'9002', '9004', '9005'}
+result = is_clicked.intersection(is_bought)
+print(f'ID klientów: {result}')
