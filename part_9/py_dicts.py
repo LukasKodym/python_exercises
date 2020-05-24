9497fc47d8530fb9668bf731b1a51948b6007891
Lukas <lukasz.kodym@gmail.com>
2020-05-24 21:38:01 +0200

    dictionaries done

--- part_9/py_dicts.py
+++ part_9/py_dicts.py
@@ -80,3 +80,46 @@ stocks = {
 print(stocks)
 stocks['BBT'] = {'Boombit': 23}
 print(stocks.values())
+
+# %%
+##
+ticker = [
+    'ALR', 'CCC', 'CDR', 'CPS', 'DNP',
+    'JSW', 'KGH', 'LPP', 'LTS', 'MBK',
+    'OPL', 'PEO', 'PGE', 'PGN', 'PKN',
+    'PKO', 'PLY', 'PZU', 'SPL', 'TPE'
+]
+print(list(enumerate(ticker)))
+
+# %%
+##
+ticker = [
+    'ALR', 'CCC', 'CDR', 'CPS', 'DNP',
+    'JSW', 'KGH', 'LPP', 'LTS', 'MBK',
+    'OPL', 'PEO', 'PGE', 'PGN', 'PKN',
+    'PKO', 'PLY', 'PZU', 'SPL', 'TPE'
+]
+
+print(dict(enumerate(ticker)))
+
+# %%
+##
+project_ids = {
+    '01': 'open',
+    '03': 'in progress',
+    '05': 'in progress',
+    '04': 'completed'
+}
+print(list(sorted(set(project_ids.values()))))
+
+# %%
+##
+stats = {'strona': 'e-smartdata.org', 'ruch': 100, 'typ': 'organiczny'}
+stats.pop('ruch')
+# del stats['ruch']
+print(stats)
+
+# %%
+##
+users = {'001': 'Marek', '002': 'Monika', '003': 'Jakub'}
+print(users.get('004', 'nieokreślony'))
