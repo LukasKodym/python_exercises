f1abdb86f3c93204e4b4394083d388cac825b144
Lukas <lukasz.kodym@gmail.com>
2020-05-25 17:52:52 +0200

    draft

---
+++ draft.py
@@ -0,0 +1,26 @@
+# %%
+##
+
+tup = (1, 5, 8, 'f', 'abc', 87)
+print(tup)
+
+print(tuple(enumerate(tup)))
+
+print(type(dict(enumerate(tup))))
+
+# %%
+##
+lst = list(tup)
+print(lst)
+
+print(dict(enumerate(lst)))
+
+# %%
+##
+ticker = [
+    'ALR', 'CCC', 'CDR', 'CPS', 'DNP',
+    'JSW', 'KGH', 'LPP', 'LTS', 'MBK',
+    'OPL', 'PEO', 'PGE', 'PGN', 'PKN',
+    'PKO', 'PLY', 'PZU', 'SPL', 'TPE'
+]
+print(list(enumerate(ticker)))
