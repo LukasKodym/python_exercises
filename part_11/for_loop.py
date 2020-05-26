6dc72720eead2d1e8ac918b9a7b510bde3288249
Lukas <lukasz.kodym@gmail.com>
2020-05-26 08:19:28 +0200

    for loop n progress

--- part_11/for_loop.py
+++ part_11/for_loop.py
@@ -21,3 +21,22 @@ for i in range(10, 99):
     if i % 11 == 0 and i % 3 != 0:
         result.append(str(i))
 print(','.join(result))
+
+# %%
+##
+items = [1, 3, 4, 5, 6, 9, 10, 17, 23, 24]
+tmp = []
+for i in items:
+    if i % 2 == 0:
+        tmp.append(i)
+print(tmp)
+
+# %%
+##
+items = [1, 5, 3, 2, 2, 4, 2, 4]
+res = []
+for i in items:
+    if items[i] != items[]:
+        res.append(i)
+print(res)
+
