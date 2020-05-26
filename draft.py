09a792d5604d46a0c187272a52c39037aa26876e
Lukas <lukasz.kodym@gmail.com>
2020-05-26 12:18:07 +0200

    for loop done

--- draft.py
+++ draft.py
@@ -69,3 +69,47 @@ for i in ticker:
     print(type(i), ': ', i)
 
 print('aaaa'.join(ticker))
+
+# %%
+##
+items = [1, 5, 3, 2, 2, 4, 2, 4]
+tmp = (dict.fromkeys(items))
+print(tmp)
+
+# %%
+##
+probabilities = [0.21, 0.91, 0.34, 0.55, 0.76, 0.02]
+tmp = []
+[tmp.append(i) for i in probabilities if i >= 0.5]
+print(tmp)
+
+# %%
+##
+text = """Python – język programowania wysokiego poziomu
+ogólnego przeznaczenia, o rozbudowanym pakiecie bibliotek
+standardowych, którego ideą przewodnią jest czytelność i
+klarowność kodu źródłowego."""
+words = text.split()
+result = []
+for word in words:
+    word.lower().replace(',', '').replace('.', '')
+    for word_2 in words:
+        if len(word_2) > 10:
+            result.append(word_2)
+print(result)
+
+# %%
+##
+indexes = [
+    'WIG', 'WIG-banki', 'WIG-budownictwo', 'WIG-CEE',
+    'WIG-chemia', 'WIG-energia', 'WIG-ESG', 'WIG-górnictwo',
+    'WIG-informatyka', 'WIG-leki', 'WIG-media', 'WIG-motoryzacja',
+    'WIG-nieruchomości', 'WIG-odzież', 'WIG-paliwa', 'WIG-Poland',
+    'WIG-spożywczy', 'WIG-telekomunikacja', 'WIG-Ukraine',
+    'WIG.GAMES', 'WIG.MS-BAS', 'WIG.MS-FIN', 'WIG.MS-PET',
+    'WIG20', 'WIG20dvp', 'WIG20lev', 'WIG20short', 'WIG20TR',
+    'WIG30', 'WIG30TR', 'WIGdiv', 'WIGtech'
+]
+
+for idx in indexes:
+    idx.find()
--- part_11/for_loop.py
+++ part_11/for_loop.py
@@ -34,9 +34,134 @@ print(tmp)
 # %%
 ##
 items = [1, 5, 3, 2, 2, 4, 2, 4]
-res = []
+items = list(dict.fromkeys(items))
+print(items)
+# or
+result = []
 for i in items:
-    if items[i] != items[]:
-        res.append(i)
+    if i not in result:
+        result.append(i)
+print(result)
+
+# %%
+##
+text = 'Python jest bardzo popularnym językiem programowania'
+words = text.split(' ')
+tmp = []
+for i in words:
+    if len(tmp) < 4:
+        tmp.append(i.lower())
+print(tmp)
+
+# or
+words = text.split(' ')
+result = []
+for idx, word in enumerate(words):
+    if idx < 4:
+        result.append(word.lower())
+print(result)
+
+# %%
+##
+probabilities = [0.21, 0.91, 0.34, 0.55, 0.76, 0.02]
+res = []
+for item in probabilities:
+    if item >= 0.5:
+        res.append(item)
 print(res)
 
+# %%
+##
+probabilities = [0.21, 0.91, 0.34, 0.55, 0.76, 0.02]
+result = []
+for prob in probabilities:
+    if prob < 0.5:
+        result.append(0)
+    elif prob >= 0.5:
+        result.append(1)
+print(result)
+
+# %%
+##
+items = ['x', 'y', 'z', 'y', 'x', 'y', 'y', 'z', 'x']
+result = {}
+for item in items:
+    if item == item:
+        result.setdefault(item, items.count(item))
+
+print(result)
+
+# or
+freq = {}
+for item in items:
+    if item not in freq.keys():
+        freq[item] = 1
+    else:
+        freq[item] += 1
+
+print(freq)
+
+# %%
+##
+text = """Python – język programowania wysokiego poziomu
+ogólnego przeznaczenia, o rozbudowanym pakiecie bibliotek
+standardowych, którego ideą przewodnią jest czytelność i
+klarowność kodu źródłowego."""
+new = text.replace('– ', '').replace(',', '').replace('.', '').replace('\n', ' ')
+words = new.split(' ')
+result = []
+for word in words:
+    if len(word) > 10:
+        result.append(word.lower())
+print(result)
+
+# or
+words = text.split()
+words = [word.lower() for word in words]
+words = [word.replace('.', '').replace(',', '') for word in words]
+words = [word for word in words if len(word) > 10]
+print(words)
+
+# %%
+##
+indexes = [
+    'WIG', 'WIG-banki', 'WIG-budownictwo', 'WIG-CEE',
+    'WIG-chemia', 'WIG-energia', 'WIG-ESG', 'WIG-górnictwo',
+    'WIG-informatyka', 'WIG-leki', 'WIG-media', 'WIG-motoryzacja',
+    'WIG-nieruchomości', 'WIG-odzież', 'WIG-paliwa', 'WIG-Poland',
+    'WIG-spożywczy', 'WIG-telekomunikacja', 'WIG-Ukraine',
+    'WIG.GAMES', 'WIG.MS-BAS', 'WIG.MS-FIN', 'WIG.MS-PET',
+    'WIG20', 'WIG20dvp', 'WIG20lev', 'WIG20short', 'WIG20TR',
+    'WIG30', 'WIG30TR', 'WIGdiv', 'WIGtech'
+]
+
+for idx in indexes:
+    if idx.count('20') > 0:
+        print(idx)
+    elif idx.count('30') > 0:
+        print(idx)
+# or
+for index in indexes:
+    if '20' in index or '30' in index:
+        print(index)
+
+# %%
+##
+gaming = {
+    '11B': 362.5,
+    'CDR': 297.0,
+    'CIG': 0.85,
+    'PLW': 318.0,
+    'TEN': 300.0
+}
+for k, v in gaming.items():
+    if v > 100:
+        print(k)
+
+# %%
+##
+names = ['Jack', 'Leon', 'Alice', '32-3c', 'Bob']
+
+for name in names:
+    if name.isalpha():
+        print(f'Hello {name}!')
