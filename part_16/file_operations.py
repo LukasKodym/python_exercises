5c1a8c3abed9b9b2491568eec8c29eee8e176cb8
Lukas <lukasz.kodym@gmail.com>
2020-06-03 19:06:40 +0200

    file operations in progress

--- part_16/file_operations.py
+++ part_16/file_operations.py
@@ -38,3 +38,31 @@ print(res)
 
 # %%
 ##
+with open('part_16\\plw_d.csv', 'r') as file:
+    content = file.read().splitlines()
+res = {}
+
+for idx, line in enumerate(content):
+    if idx == 0:
+        res.setdefault(line.split(',')[0])
+        res.setdefault(line.split(',')[-2])
+    if idx > 0:
+        res['Data'] = [line.split(',')[0] for idx, line in enumerate(content) if idx > 0]
+        res['Zamkniecie'] = [float(line.split(',')[-2]) for idx, line in enumerate(content) if idx > 0]
+
+print(res)
+
+# %%
+##
+with open('part_16\\plw_d.csv', 'r') as file:
+    content = file.read().splitlines()
+
+data = [(line.split(',')[0], line.split(',')[4]) for line in content]
+result = {
+    data[0][0]: [data[1:][i][0] for i in range(len(data) - 1)],
+    data[0][1]: [float(data[1:][i][1]) for i in range(len(data) - 1)]
+}
+print(data)
+print(data[1:][1])
+print(data[1:][1][0])
+# print(result)
