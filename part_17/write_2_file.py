19ff6ba2ce603b5d40ad9949cc911b803577c7cc
LukasKodym <lukasz.kodym@gmail.com>
2020-06-05 14:20:33 +0200

    write 2 file in progress

--- part_17/write_2_file.py
+++ part_17/write_2_file.py
@@ -8,3 +8,10 @@ even_numb = list(range(2, 20))[::2]
 with open('part_17\\num.txt', 'w') as f:
     for num in even_numb:
         f.write(str(num) + '\n')
+# %%
+##
+import json
+stocks = {'PLW': ['Playway', 350], 'BBT': ['Boombit', 22]}
+
+with open('part_17\\stocks.json', 'w') as jf:
+    json.dump(stocks, jf, indent=4)
