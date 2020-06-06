ccdc4f4b486a3152d5253f2a60dd98289a53dcb8
Lukas <lukasz.kodym@gmail.com>
2020-06-06 14:14:35 +0200

    write 2 file done

--- draft.py
+++ draft.py
@@ -176,3 +176,22 @@ techs = ['python', 'java', 'sql', 'r', 'scala']
 with open('techs.txt', 'a') as file:
     for tech in techs:
         print(tech, file=file)
+
+# %%
+##
+import csv
+
+lines = [['Bob', 'male', '27'],
+['Smith', 'male', '26'],
+['Alice', 'female', '26']]
+
+header = ['name', 'gender', 'age']
+
+with open("test.csv", "w", newline='') as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(header) # write the header
+    # write the actual content line by line
+    for l in lines:
+        writer.writerow(l)
+    # or we can write in a whole
+    # writer.writerows(lines)
--- part_17/write_2_file.py
+++ part_17/write_2_file.py
@@ -11,7 +11,31 @@ with open('part_17\\num.txt', 'w') as f:
 # %%
 ##
 import json
+
 stocks = {'PLW': ['Playway', 350], 'BBT': ['Boombit', 22]}
 
 with open('part_17\\stocks.json', 'w') as jf:
     json.dump(stocks, jf, indent=4)
+
+# %%
+##
+import csv
+
+headers = ['user_id', 'amount']
+users = [['001', '1400'], ['004', '1300'], ['007', '900']]
+
+with open('part_17\\users.csv', 'w', newline='') as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(headers)
+    for j in users:
+        writer.writerow(j)
+
+# %% or
+##
+headers = ['user_id', 'amount']
+users = [['001', '1400'], ['004', '1300'], ['007', '900']]
+
+with open('part_17\\users_1.csv', 'w') as file:
+    file.write(','.join(headers) + '\n')
+    for user in users:
+        file.write(','.join(user) + '\n')
