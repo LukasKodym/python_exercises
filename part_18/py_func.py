1a807c15fc436dbfcae0fb93a9e1e21d0cb418be
Lukas <lukasz.kodym@gmail.com>
2020-06-06 14:28:54 +0200

    built in func in progress

--- part_18/py_func.py
+++ part_18/py_func.py
@@ -9,3 +9,29 @@ print(res)
 
 # %%
 ##
+var1 = 'Python'
+var2 = ('Python')
+var3 = ('Python',)
+var4 = ['Python']
+var5 = {'Python'}
+
+print(isinstance(var1, tuple))
+print(isinstance(var2, tuple))
+print(isinstance(var3, tuple))
+print(isinstance(var4, tuple))
+print(isinstance(var5, tuple))
+
+# %%
+##
+characters = ['k', 'b', 'c', 'j', 'z', 'w']
+# characters.sort()
+print(f'Pierwsza: {min(characters)}')
+print(f'Ostatnia: {max(characters)}')
+
+# %%
+##
+ticker = ('TEN', 'PLW', 'CDR')
+full_name = ('Ten Square Games', 'Playway', 'CD Projekt')
+
+new_lst = list(zip(ticker, full_name))
+print(new_lst)
