69703a61089244f547736e4b8d5e5fb3e1bab0e1
Lukas <lukasz.kodym@gmail.com>
2020-06-07 12:39:57 +0200

    own func in progress

--- part_19/own_func.py
+++ part_19/own_func.py
@@ -7,3 +7,29 @@ def maximum(a, b):
         return a
     else:
         return b
+
+
+# %%
+##
+
+
+def maximum_2(a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    elif c > a and c > b:
+        return c
+
+
+# %%
+##
+
+
+def multi(iter_obj):
+    val = 1
+    for i in iter_obj:
+        val *= i
+    return val
+
+
