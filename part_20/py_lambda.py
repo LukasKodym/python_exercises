12f81dce593c99599156d89855a4348f35332d90
LukasKodym <lukasz.kodym@gmail.com>
2020-06-09 09:42:57 +0200

    lambda in progress

--- part_20/py_lambda.py
+++ part_20/py_lambda.py
@@ -5,9 +5,17 @@ stocks = ['playway', 'boombit', 'cd projekt']
 length = list(map(lambda item: len(item), stocks))
 print(length)
 
+
 # %%]
 ##
 
 
 def sort_list(lst):
     return sorted(lst, key=lambda x: x[1])
+
+
+# %%
+##
+
+
+func_2 = lambda x, y: x + y + 2
