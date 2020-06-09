61c7e11cca4a8fb8c1a41e03a8def6f89d7f41fa
lukas <lukasz.kodym@gmail.com>
2020-06-09 20:10:21 +0200

    lambda expression in progress

--- part_20/py_lambda.py
+++ part_20/py_lambda.py
@@ -27,5 +27,26 @@ items = [(3, 4), (2, 5), (1, 4), (6, 1)]
 items.sort(key=lambda x: x[0] ** 2 + x[1] ** 2)
 print(items)
 
-# &&
+# %%
 ##
+stocks = [
+    {'indeks': 'mWIG40', 'name': 'TEN', 'price': 304},
+    {'indeks': 'mWIG40', 'name': 'PLW', 'price': 309},
+    {'indeks': 'sWIG80', 'name': 'BBT', 'price': 22}
+]
+
+stocks.sort(key=lambda stock: stock['price'])
+
+print(stocks)
+
+# %%
+##
+stocks = [
+    {'indeks': 'mWIG40', 'name': 'TEN', 'price': 304},
+    {'indeks': 'mWIG40', 'name': 'PLW', 'price': 309},
+    {'indeks': 'sWIG80', 'name': 'BBT', 'price': 22}
+]
+
+res = list(filter(lambda item: item['indeks'] == 'mWIG40', stocks))
+
+print(res)
