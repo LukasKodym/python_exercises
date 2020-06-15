ae3c30b5fbac5a6c2ee76cdfbacd7eb77d2fbd17
LukasKodym <lukasz.kodym@gmail.com>
2020-06-15 12:21:23 +0200

    set comprehension done

--- part_22/set_compreh.py
+++ part_22/set_compreh.py
@@ -27,3 +27,13 @@ print(f'P-stwo: {len(trio_of_7) / len(omega):.2f}')
 
 # %%
 ##
+omega = {(i, j, k) for i in range(1, 7) for j in range(1, 7) for k in range(1, 7)}
+res = {(x, y, z) for (x, y, z) in omega if (x ** 2 + y ** 2 + z ** 2) % 3 == 0}
+print(f'P-stwo: {len(res) / len(omega):.4f}')
+
+# %%
+##
+omega = {(i, j, k) for i in range(1, 7) for j in range(1, 7) for k in range(1, 7)}
+res = {(x, y, z) for (x, y, z) in omega if x % 2 != 0 and y % 2 != 0 and z % 2 != 0}
+prob = round(len(res) / len(omega), 3)
+print(f'P-stwo: {prob}')
