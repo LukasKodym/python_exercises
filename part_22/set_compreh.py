c9516d1154be299c53c0d86555babbf85d613a74
LukasKodym <lukasz.kodym@gmail.com>
2020-06-15 10:39:44 +0200

    set comprehension in progress

--- part_22/set_compreh.py
+++ part_22/set_compreh.py
@@ -6,3 +6,24 @@ print(f'P-stwo: {len(sum_gt_10) / len(omega):.2f}')
 
 # %%
 ##
+desc = "Playway: Playway to producent gier komputerowych."
+
+res = desc.lower().replace(':', '').replace('.', '').split()
+
+print(len({word for word in res}))
+
+# %%
+##
+
+omega = {(i, j) for i in range(1, 7) for j in range(1, 7)}
+sum_sq_get_45 = {pair for pair in omega if pair[0] ** 2 + pair[1] ** 2 >= 45}
+print(f'P-stwo: {len(sum_sq_get_45) / len(omega):.2f}')
+
+# %%
+##
+omega = {(i, j, k) for i in range(1, 7) for j in range(1, 7) for k in range(1, 7)}
+trio_of_7 = {(x, y, z) for (x, y, z) in omega if (x + y + z) % 7 == 0}
+print(f'P-stwo: {len(trio_of_7) / len(omega):.2f}')
+
+# %%
+##
