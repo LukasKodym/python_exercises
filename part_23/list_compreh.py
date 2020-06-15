ced3bb37ece494ca666cec713b026b0437df1b10
lukas <lukasz.kodym@gmail.com>
2020-06-15 20:06:53 +0200

    list comprehension in progress

--- part_23/list_compreh.py
+++ part_23/list_compreh.py
@@ -1,4 +1,25 @@
 # %%
 ##
-with open('part_23\\gaming.txt', 'r') as file:
+with open('part_23/gaming.txt', 'r') as file:
     text = file.readlines()
+
+res = [line.replace("\n", "") for line in text if line.strip() != '']
+
+print(res)
+
+# %%
+##
+tax = 0.23
+net_price = [5.5, 4.0, 9.0, 10.0]
+
+gross_price = [round(item * (1 + tax), 2) for item in net_price]
+print(gross_price)
+
+# %%
+##
+pv = 1000
+n = 10
+rate = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07]
+
+res = [round(pv * (1 + item) ** n, 2) for item in rate]
+print(res)
