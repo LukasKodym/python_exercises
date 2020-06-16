bf9a633cb1f88a6785fe2d86b8be20173c24a957
Lukas <lukasz.kodym@gmail.com>
2020-06-16 18:23:48 +0200

    dict comprehension done

--- part_24/dict_compreh.py
+++ part_24/dict_compreh.py
@@ -11,3 +11,42 @@ stocks = ['Playway', 'CD Projekt', 'Boombit']
 res = {s: len(s) for s in stocks}
 
 print(res)
+
+# %%
+##
+stocks = {'Boombit': '001', 'CD Projekt': '002', 'Playway': '003'}
+
+res = {val: key for (key, val) in stocks.items()}
+
+print(res)
+
+# %%
+##
+stocks = {'Boombit': 22, 'CD Projekt': 295, 'Playway': 350}
+
+res = {k: v for k, v in stocks.items() if v > 100}
+
+print(res)
+
+# %%
+##
+res = [{i: i ** k for i in range(1, 10)} for k in range(1, 4)]
+
+print(res)
+
+# %%
+##
+indeks = ['WIG20', 'mWIG40', 'sWIG80']
+properties = ['liczba spółek', 'spółki', 'kapitalizacja']
+
+res = {i: {p: None for p in properties} for i in indeks}
+
+print(res)
+
+# %%
+##
+indeks = ['WIG20', 'mWIG40', 'sWIG80']
+
+res = {k: indeks[k] for k in range(0, len(indeks))}
+
+print(res)
