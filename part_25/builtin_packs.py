3a8bbead332500ace8da7cd5e26baf1ce0c495b1
LukasKodym <lukasz.kodym@gmail.com>
2020-06-17 14:46:20 +0200

    builtin packs in progress

---
+++ part_25/builtin_packs.py
@@ -0,0 +1,26 @@
+# %%
+##
+import calendar as cal
+from datetime import date as dt
+
+# %%
+##
+s = cal.calendar(2020)
+
+print(s)
+
+# %%
+##
+s = cal.month(2020, 6)
+
+print(s)
+
+# %%
+##
+d_1 = dt.date(2020, 6, 1)
+d_2 = dt.date(2020, 7, 18)
+
+print(d_2 - d_1)
+
+# %%
+##
