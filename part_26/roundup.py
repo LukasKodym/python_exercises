2e595f89f8dbf044f8bf0b47acd711966a757052
LukasKodym <lukasz.kodym@gmail.com>
2020-06-19 14:09:58 +0200

    roundup in progress

--- part_26/roundup.py
+++ part_26/roundup.py
@@ -13,3 +13,20 @@ def accuracy(y_t, y_p):
 
 
 print(accuracy(y_true, y_pred))
+
+# %%
+##
+y_true = [10, 10.5, 11.2, 10.4]
+y_pred = [10.2, 10.4, 10.8, 11.0]
+# y_true = [0, 0, 1, 1, 0, 1, 0]
+# y_pred = [0, 0, 1, 0, 0, 1, 0]
+
+
+def mea(y_true, y_pred):
+    sum = 0
+    for i, j in zip(y_true, y_pred):
+        sum += abs(y_pred[j] - y_true[i])
+    return sum / len(y_pred)
+
+
+mea(y_true, y_pred)
